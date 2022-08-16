33d46edbf964975cc9baf8fdb10dc5dff051f510
junkisai <junkisai@users.noreply.github.com>
2022-08-16 08:39:21 +0000

    GitHub Actions から差分を Push

---
+++ src/styles/tokens.d.ts
@@ -0,0 +1,28 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 16 Aug 2022 08:39:21 GMT
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value: any;
+  name?: string;
+  comment?: string;
+  themeable?: boolean;
+  attributes?: {
+    category?: string;
+    type?: string;
+    item?: string;
+    subitem?: string;
+    state?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+declare const tokens: {
+  "tokenSetOrder": {
+    "0": DesignToken
+  }
+}
---
+++ src/styles/tokens.js
@@ -0,0 +1,28 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 16 Aug 2022 08:39:21 GMT
+ */
+
+module.exports = {
+  "tokenSetOrder": {
+    "0": {
+      "value": "global",
+      "type": "other",
+      "filePath": "src/styles/output.json",
+      "isSource": true,
+      "original": {
+        "value": "global",
+        "type": "other"
+      },
+      "name": "TokenSetOrder0",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "0"
+      },
+      "path": [
+        "tokenSetOrder",
+        "0"
+      ]
+    }
+  }
+};
